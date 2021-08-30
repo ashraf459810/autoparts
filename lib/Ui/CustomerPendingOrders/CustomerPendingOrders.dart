c5b35a3624dea0477596f5aa25f4b899dbe14d99
ashraf459810 <78394984+ashraf459810@users.noreply.github.com>
2021-08-30 20:01:56 +0300

    done

--- lib/Ui/CustomerPendingOrders/CustomerPendingOrders.dart
+++ lib/Ui/CustomerPendingOrders/CustomerPendingOrders.dart
@@ -1,7 +1,5 @@
 import 'package:autopart/Ui/vendorblocs/profilebloc/profilebloc_bloc.dart';
 
-import 'package:autopart/model/PendingOrdersCustomer.dart';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
